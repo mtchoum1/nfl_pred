53e85ae675b00c491f1efa641985092bebc166d7
mtchoumi <tchoumi31mo@gmail.com>
2025-07-27 18:15:50 -0400

    Start predication equations

---
+++ Pythagorean_wins.py
@@ -0,0 +1,6 @@
+class team:
+    def __init__(self, pass_yds_for):
+        pass
+
+def pyth_win(points_for, points_aga):
+    return pow(points_for, 2.37) / (pow(points_for, 2.37) + pow(points_aga, 2.37))
